f91ff01e7fd0835284c6451e566085f81292ae5e
anaVitoria707 <anavalente3010@gmail.com>
2026-07-27 21:36:53 -0300

    Define disciplinas, turma, and matricula classes
    Added classes for disciplinas, turma, and matricula with their respective attributes.

--- model/aluno.py
+++ model/aluno.py
@@ -7,3 +7,29 @@ class Aluno:
         self.nascimento = nascimento
         self.cpf = cpf
         self.telefone = telefone
+
+Class disciplinas:
+
+    def__init__(self, id_disciplina, nome_disciplina, codigo, carga_horaria, ementa)
+        self.id_disciplina = id_disciplina
+        self.nome_disciplina = nome-disciplina
+        self.codigo = codigo
+        self.carga_horaria = carga_horaria
+        self.ementa = ementa
+
+Class turma:
+
+    def__init__(self, id_turma, codigo_turma, ano_letivo, turno, capacidade)
+        self.id_turma = id_turma
+        self.codigo_turma = codigo_turma
+        self.ano_letivo = ano_letivo
+        self.turno = turno
+        self.capacidade = capacidade
+
+Class matricula:
+
+    def__init__(self, id_matricula, data_matricula, forma_ingresso, status)
+        self.id_matricula = id_matricula
+        self.data_matricula = data_matricula
+        self.forma_ingresso = forma_ingresso
+        self.status = status
