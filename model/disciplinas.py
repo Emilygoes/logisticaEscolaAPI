8a488ad7d1d2a11d78eca7757fa729609fcaabbb
Emily goes <baamgyu.gyua@gmail.com>
2026-07-28 15:31:30 -0300

    Update disciplinas.py

--- model/disciplinas.py
+++ model/disciplinas.py
@@ -1,7 +1,7 @@
 class Disciplinas:
 
-    def __init__(self, id_disciplina, nome_disciplina, codigo, carga_horaria, ementa):
-        self.id_disciplina = id_disciplina
+    def __init__(self, codigo_disciplina, nome_disciplina, codigo, carga_horaria, ementa):
+        self.codigo_disciplina = codigo_disciplina
         self.nome_disciplina = nome-disciplina
         self.codigo = codigo
         self.carga_horaria = carga_horaria
