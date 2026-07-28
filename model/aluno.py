8e12b620a3226f159f11bbf33ec77f9ddac1d4f8
anaVitoria707 <anavalente3010@gmail.com>
2026-07-28 18:27:20 -0300

    Update aluno.py

--- model/aluno.py
+++ model/aluno.py
@@ -1,9 +1,8 @@
 class Aluno:
 
-    def __init__(self, CPF, nome, sobrenome, nascimento, cpf, telefone):
+    def __init__(self, CPF, nome, sobrenome, nascimento, telefone):
         self.CPF = CPF
         self.nome = nome
         self.sobrenome = sobrenome
         self.nascimento = nascimento
-        self.cpf = cpf
         self.telefone = telefone
