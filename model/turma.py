2e7aacdbba2f47bc2bae63015aaea231a05de9b7
Emily goes <baamgyu.gyua@gmail.com>
2026-07-28 15:32:48 -0300

    Update turma.py

--- model/turma.py
+++ model/turma.py
@@ -1,7 +1,7 @@
 class Turma:
 
-    def __init__(self, id_turma, codigo_turma, ano_letivo, turno, capacidade):
-        self.id_turma = id_turma
+    def __init__(self, numero_turma, codigo_turma, ano_letivo, turno, capacidade):
+        self.numero_turma = numero_turma
         self.codigo_turma = codigo_turma
         self.ano_letivo = ano_letivo
         self.turno = turno
