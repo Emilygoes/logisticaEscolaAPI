96e620989adc4954130dd87dca3b15ca2b9319d0
Emily goes <baamgyu.gyua@gmail.com>
2026-07-28 15:32:01 -0300

    Rename id_matricula to n_matricula in Matricula class

--- model/matricula.py
+++ model/matricula.py
@@ -1,7 +1,7 @@
 class Matricula:
 
-    def __init__(self, id_matricula, data_matricula, forma_ingresso, status):
-        self.id_matricula = id_matricula
+    def __init__(self, n_matricula, data_matricula, forma_ingresso, status):
+        self.n_matricula = n_matricula
         self.data_matricula = data_matricula
         self.forma_ingresso = forma_ingresso
         self.status = status
